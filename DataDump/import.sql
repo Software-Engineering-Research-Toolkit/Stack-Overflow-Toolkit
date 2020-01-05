c641535407e228f426619a04e09844d39f777512
Bowen Xu <maxxbw1992@gmail.com>
2020-01-05 22:10:58 +0800

    Update import.sql

--- DataDump/import.sql
+++ DataDump/import.sql
@@ -4,7 +4,6 @@ CREATE database IF NOT EXISTS SO DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8
 use SO;
 
 -- Create Table
-
 create table badges (
    Id INT NOT NULL PRIMARY KEY,
    UserId INT,
@@ -13,82 +12,82 @@ create table badges (
 );
  
 CREATE TABLE comments (
-     Id INT NOT NULL PRIMARY KEY,
-     PostId INT NOT NULL,
-     Score INT NOT NULL,
-     Text TEXT,
-     CreationDate DATETIME,
-     UserId INT 
+   Id INT NOT NULL PRIMARY KEY,
+   PostId INT NOT NULL,
+   Score INT NOT NULL,
+   Text TEXT,
+   CreationDate DATETIME,
+   UserId INT 
 );
  
 CREATE TABLE post_history (
-     Id INT NOT NULL PRIMARY KEY,
-     PostHistoryTypeId SMALLINT NOT NULL,
-     PostId INT NOT NULL,
-     RevisionGUID VARCHAR(256),
-     CreationDate DATETIME,
-     UserId INT,
-     Text TEXT
+   Id INT NOT NULL PRIMARY KEY,
+   PostHistoryTypeId SMALLINT NOT NULL,
+   PostId INT NOT NULL,
+   RevisionGUID VARCHAR(256),
+   CreationDate DATETIME,
+   UserId INT,
+   Text TEXT
 );
 
 CREATE TABLE post_links (
-    Id INT NOT NULL PRIMARY KEY,  
-    CreationDate DATETIME,
-    PostId INT,
- 	RelatedPostId INT,
- 	LinkTypeId INT
+   Id INT NOT NULL PRIMARY KEY,  
+   CreationDate DATETIME,
+   PostId INT,
+   RelatedPostId INT,
+   LinkTypeId INT
 );
  
 CREATE TABLE posts (
-    Id INT NOT NULL PRIMARY KEY,
-    PostTypeId SMALLINT,
-    AcceptedAnswerId INT,
- 	CreationDate DATETIME,
- 	Score INT NULL,
- 	ViewCount INT NULL,
- 	Body text NULL,
- 	OwnerUserId INT,
- 	LastEditorUserId INT,
- 	LastEditDate DATETIME,
- 	LastActivityDate DATETIME,
- 	Title varchar(256),
- 	Tags VARCHAR(256),
- 	AnswerCount INT,
- 	CommentCount INT,
- 	FavoriteCount INT,
- 	CommunityOwnedDate DATETIME,
-    ParentId INT       
+   Id INT NOT NULL PRIMARY KEY,
+   PostTypeId SMALLINT,
+   AcceptedAnswerId INT,
+   CreationDate DATETIME,
+   Score INT NULL,
+   ViewCount INT NULL,
+   Body text NULL,
+   OwnerUserId INT,
+   LastEditorUserId INT,
+   LastEditDate DATETIME,
+   LastActivityDate DATETIME,
+   Title varchar(256),
+   Tags VARCHAR(256),
+   AnswerCount INT,
+   CommentCount INT,
+   FavoriteCount INT,
+   CommunityOwnedDate DATETIME,
+   ParentId INT    
 );
 
 CREATE TABLE tags (
-    Id INT NOT NULL PRIMARY KEY,  
-    TagName VARCHAR(50),
-    Count INT,
- 	ExcerptPostId INT,
- 	WikiPostId INT
+   Id INT NOT NULL PRIMARY KEY,  
+   TagName VARCHAR(50),
+   Count INT,
+   ExcerptPostId INT,
+   WikiPostId INT
 );
  
 CREATE TABLE users (
-     Id INT PRIMARY KEY,
-     Reputation INT,
-     CreationDate DATETIME,
-     DisplayName VARCHAR(50) NULL,
-     LastAccessDate  DATETIME,
-     Views INT DEFAULT 0,
-     WebsiteUrl VARCHAR(256) NULL,
-     Location VARCHAR(256) NULL,
-     AboutMe TEXT NULL,
-     Age INT,
-     UpVotes INT,
-     DownVotes INT,
-     EmailHash VARCHAR(32)
+   Id INT PRIMARY KEY,
+   Reputation INT,
+   CreationDate DATETIME,
+   DisplayName VARCHAR(50) NULL,
+   LastAccessDate  DATETIME,
+   Views INT DEFAULT 0,
+   WebsiteUrl VARCHAR(256) NULL,
+   Location VARCHAR(256) NULL,
+   AboutMe TEXT NULL,
+   Age INT,
+   UpVotes INT,
+   DownVotes INT,
+   EmailHash VARCHAR(32)
 );
  
 CREATE TABLE votes (
-     Id INT NOT NULL PRIMARY KEY,
-     PostId INT NOT NULL,
-     VoteTypeId SMALLINT,
-     CreationDate DATETIME
+   Id INT NOT NULL PRIMARY KEY,
+   PostId INT NOT NULL,
+   VoteTypeId SMALLINT,
+   CreationDate DATETIME
 );
  
  
